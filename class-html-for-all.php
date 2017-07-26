cddafcfbebdcc28af631e7a375296231bdca99cb
Naveen Giri <1naveengiri@gmail.com>
2017-07-26 10:19:04 +0300

    Add some update

--- class-html-for-all.php
+++ class-html-for-all.php
@@ -24,10 +24,11 @@ class Html_For_All {
 	 * __construct Html_For_All class constructor
 	 */
 	function __construct() {
-		add_action( 'init', array( $this, 'HFA_page_permalink' ), -1 );
-		register_activation_hook( __FILE__, array( $this, 'HFA_active' ) );
-		register_deactivation_hook( __FILE__, array( $this, 'HFA_deactive' ) );
-		add_filter( 'user_trailingslashit', array( $this, 'HFA_page_slash' ),66,2 );
+		add_action( 'init', array( $this, 'hfa_page_permalink' ), -1 );
+		register_activation_hook( __FILE__, array( $this, 'hfa_active' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'hfa_deactive' ) );
+		add_filter( 'user_trailingslashit', array( $this, 'hfa_page_slash' ),66,2 );
+		// add_filter( 'post_rewrite_rules', array( $this, 'post_rewrite_rules_callback' ),99,1);
 	}
 
 	/**
@@ -49,12 +50,14 @@ class Html_For_All {
 		global $wp_rewrite;
 		if ( ! strpos( $wp_rewrite->get_page_permastruct(), '.html' ) ) {
 			$wp_rewrite->page_structure = $wp_rewrite->page_structure . '.html';
+
 		}
+		$wp_rewrite->flush_rules();
 	}
 
 	function hfa_page_slash( $string, $type ) {
 		global $wp_rewrite;
-		if ( $wp_rewrite->using_permalinks() && true === $wp_rewrite->use_trailing_slashes && 'page' === $type ) {
+		if ( $wp_rewrite->using_permalinks() && true === $wp_rewrite->use_trailing_slashes && 'page' === $type && 'post' === $type ) {
 			return untrailingslashit( $string );
 		} else {
 			return $string;
@@ -79,6 +82,15 @@ class Html_For_All {
 		global $wp_rewrite;
 		$wp_rewrite->page_structure = str_replace( '.html','',$wp_rewrite->page_structure );
 		$wp_rewrite->flush_rules();
+	}
+
+	/**
+	 * Function to add page rewrite rules for post and CPTs
+	 */
+	function post_rewrite_rules_callback( $post_rewrite ){
+		// echo "asdsad";
+		// var_export($post_rewrite);
+		$post_rewrite->permalink_structure =  '%postname%.html';
 	}
 }
 
