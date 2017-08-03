fa290a3e33e16defd043117d44dd36dad45ac04f
Naveen Giri <1naveengiri@gmail.com>
2017-08-03 09:37:19 +0300

    Fix url .html check

--- admin/class-hfa-setting.php
+++ admin/class-hfa-setting.php
@@ -92,32 +92,34 @@ class Html_For_All_AdminSettings {
 	 */
 	function hfa_settings_callback() {
 		if ( isset( $_POST['hfa_save_post_types'] ) && ! empty( $_POST['hfa_save_post_types'] ) ) {
-
 			$selected_post_type = array();
 			if ( isset( $_POST['hfa_post_types'] )  && ! empty( $_POST['hfa_post_types'] ) ) {
 				$selected_post_type = wp_unslash( $_POST['hfa_post_types'] );
-				global $wp_rewrite;
-				if ( in_array( 'post', $_POST['hfa_post_types'] ) ) {
-					$permalink_structure = get_option( 'permalink_structure' );
-					if ( ! empty( $permalink_structure ) ) {
+			}
+			global $wp_rewrite;
+			if ( ! empty( $selected_post_type ) && in_array( 'post', $selected_post_type ) ) {
+				$permalink_structure = get_option( 'permalink_structure' );
+				if ( ! empty( $permalink_structure ) ) {
+					if ( ! strpos( $permalink_structure , '.html' ) ) {
 						update_option( 'old_permalink_structure', $permalink_structure );
-						$permalink_structure = explode( '/', $permalink_structure );
-						$total_element = count( $permalink_structure );
-						if ( isset( $permalink_structure[ $total_element - 1 ] ) && empty( $permalink_structure[ $total_element - 1 ] ) ) {
-							unset( $permalink_structure[ $total_element - 1 ] );
-							$permalink_structure = implode( '/', $permalink_structure );
-							$permalink_structure .= '.html';
-							update_option( 'permalink_structure', $permalink_structure );
-						}
 					}
-				} else {
-					$old_permalink_structure = get_option( 'old_permalink_structure' );
-					if ( ! empty( $old_permalink_structure ) ) {
-						update_option( 'permalink_structure', $old_permalink_structure );
+					$permalink_structure = explode( '/', $permalink_structure );
+					$total_element = count( $permalink_structure );
+					if ( isset( $permalink_structure[ $total_element - 1 ] ) && empty( $permalink_structure[ $total_element - 1 ] ) ) {
+						unset( $permalink_structure[ $total_element - 1 ] );
+						$permalink_structure = implode( '/', $permalink_structure );
+						$permalink_structure .= '.html';
+						update_option( 'permalink_structure', $permalink_structure );
 					}
 				}
-				$wp_rewrite->flush_rules();
+			} else {
+				$old_permalink_structure = get_option( 'old_permalink_structure' );
+				$permalink_structure = get_option( 'permalink_structure' );
+				if ( ! empty( $old_permalink_structure ) && strpos( $permalink_structure , '.html' ) ) {
+					update_option( 'permalink_structure', $old_permalink_structure );
+				}
 			}
+			$wp_rewrite->flush_rules();
 
 			update_option( 'hfa_selected_post_type', $selected_post_type );
 		}
